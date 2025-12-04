c78134d9497cf9f32315ee8e022d2fb86ba83637
Brooke Eichenlaub <Brooke.eichenlaub@gmail.com>
2025-12-04 11:41:27 -0800

    commented and cleaned code, aggregated load profiles

--- merge_pipeline.py
+++ merge_pipeline.py
@@ -50,7 +50,7 @@ def load_feeder_characteristics(path="ica_data/feeder_characteristics.csv"):
     return df
 
 def process_line_zips(
-    input_dir, loading_scenario, ica_col,
+    input_dir, loading_scenario, ica_col, debug: bool = False,
 ):
     """
     Stream through PG&E division ZIPs and compute feeder-level
@@ -70,11 +70,13 @@ def process_line_zips(
         Columns: ['feederid', 'Min_ICA_OF']
     """
     records = []
-
+    #This step extracts and opens the files within the PG&E division zip files from GRIP portal
     division_zips = glob.glob(os.path.join(input_dir, "*.zip"))
     if not division_zips: 
         print(f"[ICA] No division ZIPs found in {input_dir}")
         return pd.DataFrame(columns=["feederid", "month", "hour", "loadorgen", "min_ica_of"])
+    if debug: #only process first division .zip in debug mode
+        division_zips = division_zips[:1]
 
     for div_zip_path in division_zips:
         print(f"[ICA] Processing division zip: {os.path.basename(div_zip_path)}")
@@ -84,12 +86,12 @@ def process_line_zips(
                 if not inner_name.lower().endswith(".zip"):
                     continue
 
-                # get feederid from inner ZIP name
+                # get feederid from inner ZIP name (zip files hold line sections, title = feederID)
                 feeder_id_raw = os.path.splitext(os.path.basename(inner_name))[0]
-                # Handle names like 'GICA_102530401' 
+                # Handle names like 'GICA_102530401' - GICA = Generation data, LICA = Load data
                 parts = feeder_id_raw.split("_")
                 if len(parts) > 1:
-                    # Take  part after  underscore
+                    # Take  part after  underscore since that = feederID
                     feederid = parts[-1]
                 else:
                     feederid = feeder_id_raw
@@ -112,43 +114,44 @@ def process_line_zips(
                     csv_name = csv_members[0]
                     with feeder_zip.open(csv_name) as src:
                         df = pd.read_csv(src)
-
+                #Make sure whichever column used to determine headroom is present
                 df.columns = df.columns.str.strip().str.lower()
                 if ica_col not in df.columns:
                     print(f"[ICA] Warning: '{ica_col}' not found in {csv_name}; skipping this file")
                     continue
-
+                #Ensure all other relevant columns are present. "loadorgen" used to identify whether the row represents generation or load headroom
                 required_group_cols = ["month", "hour", "loadorgen"]
                 missing = [c for c in required_group_cols if c not in df.columns]
                 if missing:
                     print(f"[ICA] Warning: missing {missing} in {csv_name}; skipping this file")
                     continue
-                
-                # filter by loading scenario
+                # filter by loading scenario. This code filters for 90th pctl loading scenario.    
                 if "loading_scenario" in df.columns and loading_scenario is not None:
                     df = df[df["loading_scenario"] == loading_scenario].copy()
+                #drop ica_sg because we care about operational flexibility constraints (ica_of)
                 if "monthly_ica_sg" in df.columns:
                     df = df.drop(columns=["monthly_ica_sg"])
-                # attach feeder id
+                # turn the feederid variable into a column, attach to every row inside zip file
                 df["feederid"] = feederid
-                df[ica_col] = pd.to_numeric(df[ica_col], errors="coerce")
-                df = df.dropna(subset=[ica_col])
+                df[ica_col] = pd.to_numeric(df[ica_col], errors="coerce") #convert to numeric, assign NA if not
+                df = df.dropna(subset=[ica_col]) #drop rows where ica_of is NA
                 if df.empty:
                     continue
                 group_cols = ["feederid", "month", "hour", "loadorgen"]
-
+                #because each feeder has many line sections, we take the min of ica_of across line sections 
                 grouped = (
                     df.groupby(group_cols, as_index=False)[ica_col]
                     .min()
                     .rename(columns={ica_col: "min_ica_of"})
                 )
-
+                #constucting combined DF from each feeder csv, keep only grouped columns + min_ica_of
                 records.append(grouped)
 
     if not records:
         raise ValueError("[ICA] No ICA values computed from division ZIPs.")
 
     feeder_ica = pd.concat(records, ignore_index=True)
+    #Some feeders have multiple files, group again
     feeder_ica = (feeder_ica.groupby(["feederid", "month", "hour", "loadorgen"], as_index=False).agg({"min_ica_of": "min"}))
 
     print(
@@ -156,37 +159,38 @@ def process_line_zips(
         f"{feeder_ica['feederid'].nunique()} feeders "
         f"across {feeder_ica['loadorgen'].unique().tolist()}."
     )
-
+    #columns: "feederid", "month", "hour", "loadorgen", "min_ica_of" 
     return feeder_ica
 
 def load_calmac_load_shapes():
     """
-    Load CALMAC hourly load shapes.
+    Load CALMAC hourly load shapes. 160 total residential load shapes, 40 per climate zone.
 
-    Expects a CSV with at least: gp, date, hour, kwh
+    CSV shoudl contain [gp (load shape identifier), date, hour, kwh]
     """
     print("Loading residential electric load shapes from CALMAC/Res_GP_Elec_2024.csv...")
     load_data = pd.read_csv("CALMAC/Res_GP_Elec_2024.csv")
     return load_data
 
 
-#1. Load spatial data
+# Load spatial data
 def load_climate_zones():
-    """Load CEC building climate zones shapefile."""
+    """Load CEC building climate zones shapefile. 
+    This is used to spacially map load shapes (GPs) and assign to correct ZIP code"""
     print("Loading climate zones...")
     climate_zones = gpd.read_file("CALMAC/Building_Climate_Zones.shp")
     return climate_zones
 
 def load_zip_polygons():
-    """Load ZIP code polygons shapefile."""
+    """Load ZIP code polygons shapefile. Used to determine which ZIPs/Climate zones belong together"""
     print("Loading ZIP polygons...")
     zips = gpd.read_file("zip_codes/zip_poly.shp")
     return zips
 
     
-def load_feeders():
+def load_feeder_shapes():
     """
-    Load feeder shapefile.
+    Load feeder shapefile. Used to Join with ZIP codes.
     """
     print("Loading feeders...")
     feeders = gpd.read_file("ica_data/FeederDetail_Voltage.shp")  
@@ -195,9 +199,8 @@ def load_feeders():
 # Map ZIP -> climate zone --> GP list
 def map_climate_zones(climate_zones: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     """
-    Add CALMAC climate zone group labels to CEC climate zones.
-
-    cz_groups mapping is from CALMAC documentation.
+    CALMAC documentation says which Climate Zones correspond to their 4 PG&E service territories.
+    Add CALMAC climate zone (extracted from (GP)) labels to CEC climate zones.
     """
     print("Mapping climate zones to CALMAC groups...")
     cz_groups = {
@@ -214,29 +217,38 @@ def map_climate_zones(climate_zones: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
 
 def process_zip_climate_mapping(zips: gpd.GeoDataFrame, climate_zones: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     """
-    Assign each ZIP a climate zone group based on centroid location.
+    Assign each ZIP a climate zone group based on the CZ that has the largest overlap area.
     Returns a GeoDataFrame with ZIP geometry and cz_groups.
     """
-    print("Processing ZIP → climate group mapping...")
+    print("Processing ZIP → climate group mapping (majority-area overlay)...")
+    # Ensure both layers are in the same CRS for area calculations
     zips = zips.to_crs(climate_zones.crs)
-    zips_centroids = zips.copy()
-    zips_centroids["geometry"] = zips_centroids.geometry.centroid
 
-    joined = gpd.sjoin(
-        zips_centroids,
+    # Intersect ZIP polygons with climate zone polygons to get overlap pieces
+    zip_cz_overlap = gpd.overlay(
+        zips,
         climate_zones[["cz_groups", "geometry"]],
-        how="left",
-        predicate="within"
+        how="intersection",
     )
-    zips["cz_groups"] = joined["cz_groups"]
-    zips_climate = zips[zips["cz_groups"].notna()].copy()
+
+    # Compute area of each overlap piece
+    zip_cz_overlap["overlap_area"] = zip_cz_overlap.geometry.area
+
+    # For each ZIP, find the climate group with the largest overlapping area
+    # Assumes ZIP_CODE uniquely identifies each ZIP polygon
+    idx = zip_cz_overlap.groupby("ZIP_CODE")["overlap_area"].idxmax()
+    majority = zip_cz_overlap.loc[idx, ["ZIP_CODE", "cz_groups"]]
+
+    # Join the primary climate group back onto the original ZIP geometries
+    zips_climate = zips.merge(majority, on="ZIP_CODE", how="left")
+    zips_climate = zips_climate[zips_climate["cz_groups"].notna()].copy()
     return zips_climate  # has ZIP_CODE, geometry, cz_groups, etc.
 
 
 def load_calmac_characteristics():
     """
     Load CALMAC residential and non-residential GP characteristics and
-    return a combined DataFrame with 'gp' and 'seg_cz' columns.
+    return a combined DataFrame with 'gp' and climate zone columns.
     """
     print("Loading CALMAC characteristics...")
     res_chars = pd.read_csv("CALMAC/res_characteristics.csv")
@@ -260,13 +272,13 @@ def zip_gp_lookup(zips_climate: gpd.GeoDataFrame, gps_all: pd.DataFrame) -> pd.D
     """
     print("Building ZIP → GP lookup (no geometry)...")
 
-    # Group GPs by CALMAC segment climate zone
+    # Group GPs by CALMAC segment climate zone, create list of relevant GPs per CZ
     gps_by_zone = gps_all.groupby("seg_cz")["gp"].apply(list).reset_index()
     gps_by_zone.rename(columns={"gp": "gp_list"}, inplace=True)
 
-    # zips_climate has a 'cz_groups' column,  match  to 'seg_cz'
+    # keep only necessary columns
     zips_small = zips_climate[["ZIP_CODE", "cz_groups"]].drop_duplicates()
-
+    # zips_climate has a 'cz_groups' column,  match  to 'seg_cz'
     zip_gp = zips_small.merge(
         gps_by_zone,
         left_on="cz_groups",
@@ -282,7 +294,7 @@ def zip_gp_lookup(zips_climate: gpd.GeoDataFrame, gps_all: pd.DataFrame) -> pd.D
     print(f"ZIP–GP pairs: {len(zip_gp)}")
     return zip_gp  
 
-#3. Aggregate Load Shapes gp x month x hour
+# Aggregate KWH consumed across gp x month x hour
 def aggregate_load_shapes():
     print("Loading CALMAC load shapes...")
     load_data = load_calmac_load_shapes()
@@ -290,15 +302,13 @@ def aggregate_load_shapes():
     # Ensure date is datetime
     load_data["date"] = pd.to_datetime(load_data["date"])
 
-    # Filter to May–October
-    #load_data = load_data[load_data["date"].dt.month.isin([5, 6, 7, 8, 9, 10])].copy()
     load_data["month"] = load_data["date"].dt.month
 
     # Aggregate to month-hour-gp
     load_month_hour = (
         load_data
         .groupby(["gp", "month", "hour"], as_index=False)
-        .agg({"kwh": "mean"})  # or "sum" depending on how you want to use it
+        .agg({"kwh": "mean"})  
     )
 
     print(
@@ -307,39 +317,38 @@ def aggregate_load_shapes():
         f"{load_month_hour['month'].nunique()} months, "
         f"{load_month_hour['hour'].nunique()} hours)"
     )
-    return load_month_hour  # gp, month, hour, kwh
+    return load_month_hour  # columns: gp, month, hour, kwh
 
-# 4. Map feeders to zips
+# Map feeders to zips
 
 def feeder_zips_map(feeders: gpd.GeoDataFrame, zips_climate: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    """Map each feeder to a ZIP using a spatial join."""
-    print("Mapping feeders to ZIPs...")
-    print("Mapping feeders → ZIPs...")
+    """Map each feeder to a ZIP using majority-area overlay."""
+    print("Mapping feeders to ZIPs (majority-area overlay)...")
 
     # Ensure same CRS
     feeders = feeders.to_crs(zips_climate.crs)
 
-    # Use centroid for assignment
-    feeders = feeders.copy()
-    feeders["centroid"] = feeders.geometry.centroid
-    feeders_centroids = feeders.set_geometry("centroid")
-
-    feeder_zip = gpd.sjoin(
-        feeders_centroids,
+    # Intersect feeder polygons with ZIP polygons to get overlap pieces
+    feeder_zip_overlap = gpd.overlay(
+        feeders[["feederid", "geometry"]],
         zips_climate[["ZIP_CODE", "geometry"]],
-        how="left",
-        predicate="within"
+        how="intersection",
     )
-    feeder_zip = feeder_zip[["feederid", "ZIP_CODE"]].dropna(subset=["ZIP_CODE"])
-    #only one zip per feeder
-    feeder_zip_map = (
-        feeder_zip.drop_duplicates(subset=["feederid"]).reset_index(drop=True)
-    )
+
+    # Compute area of each overlap piece
+    feeder_zip_overlap["overlap_area"] = feeder_zip_overlap.geometry.area
+
+    # For each feeder, find the ZIP with the largest overlapping area
+    idx = feeder_zip_overlap.groupby("feederid")["overlap_area"].idxmax()
+    feeder_zip = feeder_zip_overlap.loc[idx, ["feederid", "ZIP_CODE"]]
+
+    # Only one ZIP per feeder 
+    feeder_zip_map = feeder_zip.reset_index(drop=True)
 
     print(f"Unique feeders mapped: {feeder_zip_map['feederid'].nunique()}")
     return feeder_zip_map
 
-# 5. Pivot wide
+# Pivot wide - necessary to keep DF from exploding
 def build_feeder_gp(zip_gp: pd.DataFrame, load_month_hour: pd.DataFrame, feeder_zip_map: pd.DataFrame) -> pd.DataFrame:
     """
     Build feeder-level load features. Final shape: one row per feederid, ZIP, month, hour)
@@ -373,8 +382,8 @@ def build_feeder_gp(zip_gp: pd.DataFrame, load_month_hour: pd.DataFrame, feeder_
         index=["feederid", "month", "hour"],
         columns="gp",
         values="kwh",
-        aggfunc="mean",
-        fill_value=0.0,
+        aggfunc="mean", #shouldn't be duplicates, but pivot_wide requires an aggfunc
+        #GPs that don't belong to a feeder will be "NaN"
     ).reset_index()
 
     # flatten GP columns, rename kwh_gp
@@ -382,15 +391,22 @@ def build_feeder_gp(zip_gp: pd.DataFrame, load_month_hour: pd.DataFrame, feeder_
         f"kwh_{c}" if isinstance(c, str) and not c in {"feederid", "month", "hour"} else c
         for c in feeder_wide.columns
     ]
-    #merge Zip_code back in
+
+    # Sum across all kwh_* columns to get a single load shape per feeder-month-hour
+    gp_cols = [c for c in feeder_wide.columns if isinstance(c, str) and c.startswith("kwh_")]
+    feeder_wide["load"] = feeder_wide[gp_cols].sum(axis=1)
+
+    # Keep only aggregated load plus keys
+    feeder_wide = feeder_wide[["feederid", "month", "hour", "load"]]
+
+    # Merge ZIP_CODE back in
     feeder_wide = feeder_wide.merge(
         feeder_zip_map,
         on="feederid",
         how="left"
     )
 
-    print(f"Feeder-wide feature rows: {len(feeder_wide)} "
-          f"and {len(feeder_wide.columns)} columns")
+    print(f"Feeder-wide feature rows: {len(feeder_wide)} with aggregated load shape")
     return feeder_wide
 
 def load_ev_data(ev_path: str = "EV_Pop_Growth_23_24.csv") -> pd.DataFrame:
@@ -450,7 +466,7 @@ def attach_ev_to_feeders(feeder_features: pd.DataFrame, ev_df: pd.DataFrame) -> 
     print(f"Rows after EV merge:  {len(merged):,}")
     return merged
 
-def main():
+def main(debug: bool = False):
     # Load  climate + ZIP
     climate_zones = load_climate_zones()
     climate_zones = map_climate_zones(climate_zones)
@@ -466,7 +482,7 @@ def main():
     load_month_hour = aggregate_load_shapes()
 
     # 4. Load feeders + map to ZIPs
-    feeders = load_feeders()
+    feeders = load_feeder_shapes()
     feeder_zip_map = feeder_zips_map(feeders, zips_climate)
 
     # 5. Build feeder × month-hour × GP load matrix
@@ -479,6 +495,7 @@ def main():
         input_dir="ica_data/raw_zips",
         loading_scenario=90,
         ica_col="hourly_ica_of",
+        debug=debug,
     )
 
     # This will duplicate rows, one for load and one for gen
@@ -505,15 +522,6 @@ def main():
         how="left"
     )
 
-    
-
-
-#the above pipeline keeps only generation rows, no load rows from the ICA data. This is not intentional, but load rows do not use the
-#hourly_ica_of column (all NAs). Therefore, those are dropped when we filter out non-numeric values. 
-#We can fix this later if we decide we want load rows.
-# ---- NEW: Load and merge weather data ----
-
-    # ---- NEW: Load and merge weather data ----
     print("Loading weather data...")
     weather = pd.read_csv("weather/2024_weather_cleaned.csv")
 
@@ -550,6 +558,6 @@ def main():
     feeder_features.to_csv("outputs/feeder_load_features.csv", index=False)
     print("Saved outputs/feeder_load_features.csv")
 
-    return feeder_features   # ← correctly inside main()
+    return feeder_features   
 if __name__ == "__main__":
-    feeder_features = main()
+    feeder_features = main(debug=False) #debug = TRUE tests pipeline with less data
